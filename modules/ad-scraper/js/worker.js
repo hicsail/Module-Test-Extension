68aa5c06a4142e12a548848e26464f8419da74aa
wenhwang97 <wenhwang@bu.edu>
2023-02-09 16:30:51 -0500

    feat: add scraper for sites using prebid

--- modules/ad-scraper/js/worker.js
+++ modules/ad-scraper/js/worker.js
@@ -1,46 +1,107 @@
 /* global chrome, handleMessage, registerCustomModule, registerMessageHandler */
 const recordHTML = function (request, sender, sendResponse) {
-    console.log('[Ad Scraper] Recording Ads for ' + request.url + '...')
+  console.log("[Ad Scraper] Recording Ads for " + request.url + "...");
 
-    if (request.content === 'record_Ads') {
-        const payload = {
-            'url*': request.url,
-            'page-title*': request.pageTitle,
-            Ads: []
+  if (request.content === "record_Ads") {
+    const payload = {
+      "url*": request.url,
+      "page-title*": request.pageTitle,
+      Ads: [],
+    };
+
+    chrome.Ads.getAll(
+      {
+        url: request.url,
+      },
+      function (Ads) {
+        Ads.forEach(function (ad) {
+          console.log(ad.name + " --> " + ad.name);
+          console.log(ad);
+
+          payload.Ads.push(ad);
+        });
+
+        if (payload.Ads.length > 0) {
+          const newRequest = {
+            content: "record_data_point",
+            generator: "browser-Ads",
+            payload: payload, // eslint-disable-line object-shorthand
+          };
+
+          handleMessage(newRequest, sender, sendResponse);
         }
+      }
+    );
 
-        chrome.Ads.getAll({
-            url: request.url
+    return true;
+  }
+
+  return false;
+};
+
+const fetchPrebid = function (request, sender, sendResponse) {
+  if (request.content === "fetch_prebid") {
+    console.log("[Ad Scraper] Fetching prebid for " + request.url + "...");
+
+    const tabId = sender.tab.id;
+    const frameId = sender.frameId;
+
+    return chrome.scripting
+      .executeScript({
+        target: {
+          tabId,
+          frameIds: [frameId],
         },
-            function (Ads) {
-                Ads.forEach(function (ad) {
-                    console.log(ad.name + ' --> ' + ad.name)
-                    console.log(ad)
+        world: "MAIN",
+        func: function () {
+          const findAndReturnPrebid = function () {
+            // possible prebid variable names
+            const prebidNames = ["pbjs", "owpbjs", "fsprebid"];
 
-                    payload.Ads.push(ad)
-                })
+            for (const name of prebidNames) {
+              // when found the prebid variable, return the object and any required function calls
+              if (window[name] !== undefined) {
+                const prebid = window[name];
 
-                if (payload.Ads.length > 0) {
-                    const newRequest = {
-                        content: 'record_data_point',
-                        generator: 'browser-Ads',
-                        payload: payload // eslint-disable-line object-shorthand
-                    }
+                return {
+                  prebid: prebid,
+                  config: prebid.getConfig(),
+                  bid_responses: prebid.getBidResponses(),
+                  winning_bids: prebid.getAllWinningBids(),
+                  all_winning_bids: prebid.getAllPrebidWinningBids(),
+                };
+              }
+            }
 
-                    handleMessage(newRequest, sender, sendResponse)
-                }
-            })
+            return null;
+          };
 
-        return true
-    }
+          try {
+            const result = findAndReturnPrebid();
 
-    return false
-}
+            return {
+              prebid: result,
+            };
+          } catch (e) {
+            // ignore CSP errors
+            console.warn("[Ad Scraper] eval error", e);
+          }
 
+          return {
+            prebid: null,
+          };
+        },
+      })
+      .then((response) => {
+        sendResponse(response[0].result);
+      });
+  }
+
+  return false;
+};
 
 registerCustomModule(function (config) {
-    console.log('[Ad Scraper] Service worker initialized.')
-    registerMessageHandler('Ad_scraper', recordHTML)
-})
-
-
+  console.log("[Ad Scraper] Service worker initialized.");
+  registerMessageHandler("Ad_scraper", recordHTML);
+  registerMessageHandler("fetch_prebid", fetchPrebid);
+});
